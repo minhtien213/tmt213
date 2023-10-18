b0cbc3a6e04393b4d7a743e3b312e3f27ce2bf69
minhtien213 <minhtien213@gmail.com>
2023-10-18 22:52:24 +0900

    FixbtnUpdate

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -2,87 +2,87 @@
 var app = {
     timelines: [
                 {
-                    id: 17,
+                    id: 0,
                     image: './assets/img/timelineimg/embe.jpg',
                     content: '20/08/2023 - Tèng teng...Cái gì đây Hêuiuuu.'
                 },
                 {
-                    id: 16,
+                    id: 1,
                     image: './assets/img/timelineimg/dkkh.jpg' ,
                     content: '02/08/2023 - Đăng kí kết hôn tại Nhật bản và chính thức trở thành Vợ Chồng.'
                 },
                 {
-                    id: 15,
+                    id: 2,
                     image: './assets/img/timelineimg/cauhon.jpg' ,
                     content: '01/08/2023 - Cầu hôn Hêuiuuu.'
                 },
                 {
-                    id: 14,
+                    id: 3,
                     image: './assets/img/timelineimg/damhoi.jpg' ,
                     content: '10.03.2023 - Hai gia đình gặp mặt và tổ chức đám hỏi cho 2 đứa.'
                 },
                 {
-                    id: 13,
+                    id: 4,
                     image: './assets/img/timelineimg/nhatban4.jpg' ,
                     content: 'Cùng nhau trải qua cuộc sống tại Nhật bản dù không được ở cùng nhau.'
                 },
                 {
-                    id: 12,
+                    id: 5,
                     image: './assets/img/timelineimg/dixe.jpg' ,
                     content: 'Mỗi tháng vượt gần 100km để gặp Hêuiuuu.'
                 },
                 {
-                    id: 11,
+                    id: 6,
                     image: './assets/img/timelineimg/gaplai.jpg' ,
-                    content: '11.06.2022 - Lần đầu gặp lại nhau tại Nhật Bản sau bao năm yêu xaaaa.'
+                    content: '11.06.2022 - Lần đầu gặp lại nhau tại Nhật Bản sau bao năm yêu xa.'
                 },
                 {
-                    id: 10,
+                    id: 7,
                     image: './assets/img/timelineimg/nhatban3.jpg' ,
                     content: ' 01.05.2022 - Anh đã sang Nhật cùng Hêuiuu.'
                 },
                 {
-                    id: 9,
+                    id: 8,
                     image: './assets/img/timelineimg/nhatban2.jpg' ,
                     content: 'Quyết tâm sang nhật cùng Hêuiuuu.'
                 },
                 {
-                    id: 8,
+                    id: 9,
                     image: './assets/img/timelineimg/nhatban1.jpg' ,
-                    content: 'Hêuiuuu ở Nhật bản và chúng ta lại yêu xaa.'
+                    content: 'Hêuiuuu ở Nhật bản và chúng ta lại yêu xa.'
                 },
                 {
-                    id: 7,
+                    id: 10,
                     image: './assets/img/timelineimg/nhatban.jpg' ,
                     content: '26/08/2018 - Ngày Hêuiuuu sang nhật làm việc.'
                 },
                 {
-                    id: 6,
+                    id: 11,
                     image: './assets/img/timelineimg/totnghiep.jpg' ,
                     content: 'Cùng nhau tốt nghiệp ra trường và cũng đến lúc yêu xa'
                 },
                 {
-                    id: 5,
+                    id: 12,
                     image: './assets/img/timelineimg/henho.jpg' ,
                     content: 'Những lần hẹn hò đầu tiên của 2 đứa.'
                 },
                 {
-                    id: 4,
+                    id: 13,
                     image: './assets/img/timelineimg/dongy.jpg' ,
                     content: '09.05 2014 - Ngày đồng ý yêu nhau sau 3 tháng "LÀM GIÁ" của Hêuiuuu.'
                 },
                 {
-                    id: 3,
+                    id: 14,
                     image: './assets/img/timelineimg/timhieu.jpg' ,
                     content: 'Sau 3 tháng tìm hiểu nhau và...'
                 },
                 {
-                    id: 2,
+                    id: 15,
                     image: './assets/img/timelineimg/gapmat.jpg' ,
                     content: 'Lần đầu tiên gặp mặt Hêuiuuu.'
                 },
                 {
-                    id: 1,
+                    id: 16,
                     image: './assets/img/timelineimg/nhantin.jpg' ,
                     content: 'Ngày đầu tiên nhắn tin làm quen Hêuiuuu.'
                 },
@@ -96,10 +96,10 @@ var app = {
                         <img class="timeline_img" src="${timeline.image}">
                         <p class="label_content">${timeline.content}</p>
                         <div class="option">
-                            <i data-id = "${timeline.id}" data-index = "${index}" class="ti-angle-down optionIcon"></i>
+                            <i data-id = "${timeline.id}" class="ti-angle-down optionIcon"></i>
                             <ul class="optionList">
-                                <li data-id = "${timeline.id}" data-index = "${index}" class="editBtn">Edit</li>
-                                <li data-id = "${timeline.id}" data-index = "${index}" class="deleteBtn">Delete</li>
+                                <li data-id = "${timeline.id}" class="editBtn">Edit</li>
+                                <li data-id = "${timeline.id}" class="deleteBtn">Delete</li>
                             </ul>
                         </div>
                         <i data-id="${timeline.id}" class="ti-heart like"></i>
@@ -122,6 +122,7 @@ var app = {
                 icon.classList.add('clicked');
                 }
             })
+            
     },
 
     handleEvents: function () {
@@ -146,7 +147,7 @@ var app = {
             
             //Click Delete button
             if (target.classList.contains('deleteBtn')) {
-                var index = target.getAttribute('data-index');
+                var index = target.getAttribute('data-id');
                 app.timelines.splice(index, 1);
                 app.render();
             }
@@ -166,8 +167,8 @@ var app = {
             //Click Edit button
             if (target.classList.contains('editBtn')) {
                 var index = target.getAttribute('data-id');
+                localStorage.setItem('index', index);
                 var parentDiv = target.closest('.elementsDiv'); // Lấy phần tử cha .elementsDiv
-                // console.log(parentDiv)
                 var labelContent = parentDiv.querySelector('.label_content'); // Lấy phần tử p.label_content
                 var textArea = document.getElementById('inputContent'); // Lấy textarea trong modal
                 textArea.value = ''; // Xóa giá trị của textarea
@@ -179,9 +180,10 @@ var app = {
 
             //Click Update button
             btnUpdate.addEventListener('click', function () {
-
-                // logic code for update
-                
+                var index = localStorage.getItem('index');
+                var textArea = document.getElementById('inputContent');
+                app.timelines[index].content = textArea.value
+                app.render()
                 optionLists.forEach(function(option) {
                     option.style.display = 'none'; // tắt hết option khác
                 });
@@ -203,13 +205,7 @@ var app = {
                 } 
         });
 
-        // Close the modalCreate / Update
-        controlCreate.addEventListener('click', function(){
-            modal.style.display = "none";
-            var textArea = $('#inputContent');
-            textArea.value = '';
-        });
-        // Close the modalCreate / Update
+        // Open the modalCreate / Update
         addTimeline.addEventListener('click', function(){
             modal.style.display = "flex";
             var textArea = $('#inputContent');
@@ -217,6 +213,20 @@ var app = {
             btnCreate.style.display = 'block'; // Ẩn nút Create
             btnUpdate.style.display = 'none'; // Hiện nút Update
         });
+
+        // Close the modalCreate / Update
+        controlCreate.addEventListener('click', function(e){
+            modal.style.display = "none";
+            var textArea = $('#inputContent');
+            textArea.value = '';
+            localStorage.removeItem('index')
+            optionLists.forEach(function(option) {
+                option.style.display = 'none'; // tắt hết option khác
+            });
+            modal.style.display = "none"; // Đóng modal
+        });
+
+        
     },
 
     start: function () {
